2d8c4182865e26c6229f3c51e2befb5b1b5ef5f4
Yuan Zhong <yuanzhong@225-3.synergydsl.com>
2020-10-28 10:53:09 -0400

    potential error

--- R/prepare_data.R
+++ R/prepare_data.R
@@ -62,9 +62,9 @@ prepare_data = function(data, unique_subject_id, time_name, response_name,
   N <- length(unique(data$ID)) # total number of unique subjects
 
   #convert time and response to numeric
-  # data[, c(time_name, response_name)] <- lapply(data[, c(time_name,
-  #                                                        response_name)],
-  #                                               function(x) as.numeric(as.character(x)))
+  data[, c(time_name, response_name)] <- lapply(data[, c(time_name,
+                                                         response_name)],
+                                                function(x) as.numeric(as.character(x)))
 
   # convert group id to be numeric
   if (!is.null(group_name)) {
